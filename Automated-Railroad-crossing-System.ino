a894d071ee76ceaabe531054dc3be6a4266dc4b5
akib hosen khan <36074470+akib360@users.noreply.github.com>
2018-03-02 14:51:07 +0600

    Add files via upload

---
+++ Automated-Railroad-crossing-System.ino
@@ -0,0 +1,64 @@
+/*___________________________________________________________________________________
+| Author : Akib Hosen Khan                                                           |                         
+|          Dept of Information & Communication Engineering                           |
+|          Islamic University, Bangladesh                                            |
+|                                 Smart RailWay System                               |
+| HardWare: Arduino Uno, DC motor, some LED as Light Signals, IR Obstacle Sensors,   |
+|           L293D motor driver                                                       |
+|                                                                                    |
+|___________________________________________________________________________________*/
+
+
+
+
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  pinMode(7,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  digitalWrite(13,HIGH);
+  }
+
+void loop() {
+  if(digitalRead(8)==LOW){
+    //delay(1000);
+    digitalWrite(7, HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    delay(20);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    while(digitalRead(9)==HIGH){
+    }
+    digitalWrite(7, LOW);
+    digitalWrite(13,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(2,LOW);
+    delay(20);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    delay(200);
+  }
+  else if(digitalRead(9)==LOW){
+    digitalWrite(7,HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    delay(150);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    while(digitalRead(8)==HIGH){
+    }
+    digitalWrite(7,LOW);
+    digitalWrite(13,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(2,HIGH);
+    delay(200);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    delay(200);
+  }
+}
